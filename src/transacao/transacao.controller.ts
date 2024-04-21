8dbb749b5abf5f8aa0b753dabca711e4bc5c0a33
adsonb87 <adsonb87@gmail.com>
2024-04-20 23:24:10 -0300

    Endpoints de transações

--- src/transacao/transacao.controller.ts
+++ src/transacao/transacao.controller.ts
@@ -7,10 +7,12 @@ import {
   Param,
   Delete,
   NotFoundException,
+  Query,
 } from '@nestjs/common';
 import { TransacaoService } from './transacao.service';
 import { ApiTags } from '@nestjs/swagger';
 import { UpdateTransacaoDto } from './dto/update-transacao.dto';
+import { query } from 'express';
 
 @ApiTags('Transacao')
 @Controller('/api/v1/transacao')
@@ -34,6 +36,32 @@ export class TransacaoController {
   async findAll() {
     try {
       const retorno = await this.transacaoService.findAll();
+      return {
+        message: 'sucesso',
+        result: retorno,
+      };
+    } catch (error) {
+      throw new NotFoundException(`${error.message}`);
+    }
+  }
+
+  @Get('/usuario/:id')
+  async findTransacaoUsuario(@Param('id') id: string) {
+    try {
+      const retorno = await this.transacaoService.findTransacaoUsuario(+id);
+      return {
+        message: 'sucesso',
+        result: retorno,
+      };
+    } catch (error) {
+      throw new NotFoundException(`${error.message}`);
+    }
+  }
+
+  @Get('/tipo')
+  async findTransacaoTipo(@Query() params) {
+    try {
+      const retorno = await this.transacaoService.findTransacaoTipo(params);
       return {
         message: 'sucesso',
         result: retorno,
